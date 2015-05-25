e322371449ff458b2d4e8d8e517ff895b6b9307d
Arnaud474 <arnaud.girardin444@gmail.com>
2015-05-25 11:33:16 -0400

    Ajout des autres scripts

---
+++ Arnaud/B64/Modification_IP_HYPERV_SR.ps1
@@ -0,0 +1,20 @@
+﻿#Nom : Arnaud Girardin
+#Date : 2015-05-04
+#Description : Creation des dossiers pour le DFS
+#Source : Serveur Reel
+#Modifications : Serveur Reel
+
+#Rename Ordinateur
+
+#Obtention du nom de la carte reseau
+$name = Get-NetAdapter -Name "Ethernet 4";
+$name = $name.Name;
+
+#Changement de l'adresse de la carte
+New-NetIPAddress -InterfaceAlias $name -PrefixLength 16 -DefaultGateway 10.57.1.1 -IPAddress 10.57.64.23; 
+
+#Assignation du DNS
+Set-DnsClientServerAddress -InterfaceAlias $name -ServerAddresses 10.57.4.28, 10.57.4.29;
+
+#Hyper-V
+Install-WindowsFeature Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Restart
---
+++ Arnaud/B64/Parametrage_HYPERV_SR.ps1
@@ -0,0 +1,78 @@
+﻿#Nom : Arnaud Girardin
+#Date : 2015-05-07
+#Description : Parametrage de Hyper-V
+#Source : Serveur Reel
+#Modifications : Serveur Reel
+
+#On va chercher la carte
+if($name = Get-NetAdapter -Name "Ethernet 4")
+{
+    $name = $name.Name;
+
+    #Rename des cartes reseau
+    Rename-NetAdapter -Name $name -NewName "WAN";
+
+    $diskPath = "C\_VirDisque";
+    $machinePath = "C\_VirOrdi";
+
+    #Changement des paths
+    Set-VMHost -VirtualHardDiskPath $diskPath -VirtualMachinePath $machinePath;
+
+    #Creation des commutateurs
+    New-VMSwitch -Name Comm_INTERNE -SwitchType Internal
+    New-VMSwitch -Name Comm_EXTERNE -NetAdapterName WAN
+
+    #Modification Adresse IP Interne
+    $name = Get-NetAdapter -Name "vEthernet (Comm_INTERNE)";
+    $name = $name.Name;
+
+    #Changement de l'adresse de la carte
+    New-NetIPAddress -InterfaceAlias $name -PrefixLength 24 -IPAddress 192.168.64.100; 
+
+    #Creation des ordinateurs virtuels
+
+    $vmName = "SERVEUR_1"
+
+    #Serveur 1
+    New-VM -Name $vmName -Generation 1 -MemoryStartupBytes 4096MB -VHDPath $diskPath"\S1_PROJET.vhdx" 
+
+    #On enleve la carte de base
+    Remove-VMNetworkAdapter -VMName $vmName;
+
+    #On ajoute le commutateur
+    Add-VMNetworkAdapter -VMName $vmName -SwitchName Comm_INTERNE -IsLegacy $true
+
+    #Caps lock
+    Set-VMBios -VMName $vmName -EnableNumLock 
+
+    $vmName = "SERVEUR_2"
+
+    #Serveur 2
+    New-VM -Name $vmName -Generation 1 -MemoryStartupBytes 4096MB -VHDPath $diskPath"\S2_PROJET.vhdx" 
+
+    #On enleve la carte de base
+    Remove-VMNetworkAdapter -VMName $vmName;
+
+    #On ajoute le commutateur
+    Add-VMNetworkAdapter -VMName $vmName -SwitchName Comm_INTERNE -IsLegacy $true
+
+    #Caps lock
+    Set-VMBios -VMName $vmName -EnableNumLock 
+
+	$vmName = "ROUTEUR"
+
+    #Routeur
+    New-VM -Name $vmName -Generation 1 -MemoryStartupBytes 512MB -VHDPath $diskPath"\ROUTEUR_PROJET.vhd" 
+
+    #On enleve la carte de base
+    Remove-VMNetworkAdapter -VMName $vmName;
+
+    #On ajoute le commutateur
+    Add-VMNetworkAdapter -VMName $vmName -SwitchName Comm_INTERNE -IsLegacy $true
+	Add-VMNetworkAdapter -VMName $vmName -SwitchName Comm_EXTERNE -IsLegacy $true
+
+}
+else
+{
+    Write-Host "Implossible de trouver la carte !"
+}
