aad22f9a1ed56dc8de491379e2fa2ffa78d40783
William Perreault <willyjames.music@gmail.com>
2015-05-29 09:33:11 -0400

    Update CréationDossiersPartage.ps1
    Fix Erreur de CopyPaste

--- JuanTanamera/CréationDossiersPartage.ps1
+++ JuanTanamera/CréationDossiersPartage.ps1
@@ -3,13 +3,13 @@
 # Date: 28 mai 2015
 #
 # Objectif: 
-# 	- la création des dossier, 
+# 	- la crÃ©ation des dossier, 
 #	- les autorisations NTFS, 
-#	- la création des partages
+#	- la crÃ©ation des partages
 #	- les autorisations de partage
 #
-# Serveur d'éxécution: SERVEUR 1 (Contrôleur de Domaine)
-# Serveurs modifiés: SERVEUR 1 (Contrôleur de Domaine)
+# Serveur d'Ã©xÃ©cution: SERVEUR 1 (ContrÃ´leur de Domaine)
+# Serveurs modifiÃ©s: SERVEUR 1 (ContrÃ´leur de Domaine)
 ######################################################
 
 $nomDomaine = "HYRULE.PRO"
@@ -18,40 +18,40 @@ $nomServeur1 = "Gerudo"
 $nomServeur2 = "Ganondorf"
 
 $dossierPROGRAMMATION = "\\$nomServeur1\C$\_CEGAT_PRO\PROGRAMMATION"
-$dossierINTEGRATION = "\\$nomServeur1\C$\_CEGAT_PRO\INTÉGRATION"
+$dossierINTEGRATION = "\\$nomServeur1\C$\_CEGAT_PRO\INTÃ‰GRATION"
 $dossierANALYSE = "\\$nomServeur1\C$\_CEGAT_PRO\ANALYSE"
 $dossierTEST = "\\$nomServeur2\C$\_CEGAT_PRO\TEST"
 
 
 ##
 # Attribut les droits aux gestionnaires
-# enlève les droits aux Utilisateurs, Utilisateurs authentifiés, 
+# enlÃ¨ve les droits aux Utilisateurs, Utilisateurs authentifiÃ©s, 
 # Utilisateurs du domaine
-# @param: dossier à modifier
+# @param: dossier Ã  modifier
 ##
 function NTFSGeneral($dossier){
 
 	icacls  $dossier  /grant "$nomDomaine\Gestionnaires:(CI)(OI)(F)"
 	icacls  $dossier  /grant "$nomDomaine\Administrateurs:(CI)(OI)(F)"
 	icacls  $dossier  /remove "$nomDomaine\Utilisateurs" /T
-	icacls  $dossier  /remove "$nomDomaine\Utilisateurs authentifiés" /T
+	icacls  $dossier  /remove "$nomDomaine\Utilisateurs authentifiÃ©s" /T
 	icacls  $dossier  /remove "$nomDomaine\Utilisateurs du domaine" /T
 }
 
 ##
-# Créé un nouveau partage selon 
-# les paramètres spécifiés
+# CrÃ©Ã© un nouveau partage selon 
+# les paramÃ¨tres spÃ©cifiÃ©s
 # @param: serveur - Le nom du serveur cible
-# @param: chemin - à partir du C (Le lecteur C est implied)
+# @param: chemin - Ã  partir du C (Le lecteur C est implied)
 ##
 function nouveauPartage($serveur, $nomPartage, $cheminPartage){
 	#Remote Session
 	$c = New-CimSession -ComputerName $serveur;
 
-    # On essaie de le supprimer s'il est déjà existant
+    # On essaie de le supprimer s'il est dÃ©jÃ  existant
     Remove-SmbShare -Name $nomPartage -CimSession $c;
     
-	#CRÉATION DU PARTAGE
+	#CRÃ‰ATION DU PARTAGE
 	Write-Host "Creation du partage $nomPartage sur $serveur ..."
 	New-SmbShare -Path $cheminPartage `
 				-Name $nomPartage `
@@ -59,14 +59,14 @@ function nouveauPartage($serveur, $nomPartage, $cheminPartage){
 				-FolderEnumerationMode AccessBased
 				-CimSession $c  `
 
-	Write-Host "Creation du partage $nomPartage sur $serveur RÉUSSIE " -ForegroundColor Green
+	Write-Host "Creation du partage $nomPartage sur $serveur RÃ‰USSIE " -ForegroundColor Green
 }
 
 
 
 # MAIN SCRIPT #
 
-#Création des dossiers
+#CrÃ©ation des dossiers
 New-Item -path $dossierPROGRAMMATION -ItemType Directory
 New-Item -path $dossierINTEGRATION  -ItemType Directory
 New-Item -path $dossierANALYSE -ItemType Directory
@@ -79,35 +79,35 @@ NTFSGeneral($dossierANALYSE)
 NTFSGeneral($dossierTEST)
 
 #autorisations ANALYSE
-icacls  $dossierANALYSE  /grant "$nomDomaine\Intégrateurs:(CI)(OI)(M)"
+icacls  $dossierANALYSE  /grant "$nomDomaine\IntÃ©grateurs:(CI)(OI)(M)"
 icacls  $dossierANALYSE  /grant "$nomDomaine\Developpeurs 3D:(CI)(OI)(RX)"
 icacls  $dossierANALYSE  /grant "$nomDomaine\Designers WEB:(CI)(OI)(RX)"
 icacls  $dossierANALYSE  /grant "$nomDomaine\Testeurs d'outils:(CI)(OI)(RX)"
 icacls  $dossierANALYSE  /grant "$nomDomaine\Testeurs de Jeux:(CI)(OI)(RX)"
 
-#autorisations INTÉGRATION
+#autorisations INTÃ‰GRATION
 icacls  $dossierINTEGRATION  /grant "$nomDomaine\Designers WEB:(CI)(OI)(M)"
 icacls  $dossierINTEGRATION  /grant "$nomDomaine\Developpeurs 3D:(CI)(OI)(RX)"
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Intégrateurs:(CI)(OI)(RX)"
+icacls  $dossierINTEGRATION  /grant "$nomDomaine\IntÃ©grateurs:(CI)(OI)(RX)"
 icacls  $dossierINTEGRATION  /grant "$nomDomaine\Testeurs d'outils:(CI)(OI)(RX)"
 icacls  $dossierINTEGRATION  /grant "$nomDomaine\Testeurs de Jeux:(CI)(OI)(RX)"
 
 #autorisations PROGRAMMATION
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Designers .NET:(CI)(OI)(M)"
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Developpeurs 3D:(CI)(OI)(RX)"
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Designers WEB:(CI)(OI)(RX)"
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Testeurs d'outils:(CI)(OI)(RX)"
-icacls  $dossierINTEGRATION  /grant "$nomDomaine\Testeurs de Jeux:(CI)(OI)(RX)"
+icacls  $dossierPROGRAMMATION  /grant "$nomDomaine\Designers .NET:(CI)(OI)(M)"
+icacls  $dossierPROGRAMMATION  /grant "$nomDomaine\Developpeurs 3D:(CI)(OI)(RX)"
+icacls  $dossierPROGRAMMATION  /grant "$nomDomaine\Designers WEB:(CI)(OI)(RX)"
+icacls  $dossierPROGRAMMATION  /grant "$nomDomaine\Testeurs d'outils:(CI)(OI)(RX)"
+icacls  $dossierPROGRAMMATION  /grant "$nomDomaine\Testeurs de Jeux:(CI)(OI)(RX)"
 
 #autorisations TEST
 icacls  $dossierTEST  /grant "$nomDomaine\Designers .NET:(CI)(OI)(M)"
 icacls  $dossierTEST  /grant "$nomDomaine\Developpeurs 3D:(CI)(OI)(RX)"
-icacls  $dossierTEST  /grant "$nomDomaine\Intégrateurs:(CI)(OI)(RX)"
+icacls  $dossierTEST  /grant "$nomDomaine\IntÃ©grateurs:(CI)(OI)(RX)"
 icacls  $dossierTEST  /grant "$nomDomaine\Testeurs d'outils:(CI)(OI)(RX)"
 icacls  $dossierTEST  /grant "$nomDomaine\Testeurs de Jeux:(CI)(OI)(RX)"
 
 
-#Création des Partages
+#CrÃ©ation des Partages
 nouveauPartage -serveur $serveur1 -nomPartage "partage_PROGRAMMATION" -cheminPartage $dossierPROGRAMMATION
 nouveauPartage -serveur $serveur1 -nomPartage "partage_INTEGRATION" -cheminPartage $dossierINTEGRATION
 nouveauPartage -serveur $serveur1 -nomPartage "partage_ANALYSE" -cheminPartage $dossierANALYSE
